5e146850d0e27a233c817c773c4ce43c18e48176
Timothy Handy <timsfilestorage@gmail.com>
2024-09-21 23:09:52 -0500

    Little commits. Includes EfficientNet-B0 weights.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='ddrcv',
+    version='0.1.0',
+    author='Zvidun',
+    author_email='none.of@your.business',
+    description='Computer vision utilities to interrogate video feed for tournament enhancement',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tahandy/ddrcv',
+    packages=['ddrcv'],
+    python_requires='>=3.10',
+)
